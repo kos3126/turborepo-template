1cfb7e7fc5291a0c94a7041677024e6f141f4ee5
KosM <kosukemorimoto1@gmail.com>
2022-09-24 14:17:42 +0900

    Added path option to tsconfig

--- apps/next-next-app/src/pages/_app.tsx
+++ apps/next-next-app/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@/styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
